40a384425390f99d7207d3567e4346784b4e8365
Luke Teal <l.teal21@gmail.com>
2021-08-03 15:17:55 -0400

    switch to conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import './css/normalize v8.0.1.css';
+import './css/index.css';
 
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import Home from './pages/home';
-import Profile from './pages/profile';
-import Header from './components/header';
-import Footer from './components/footer';
-
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
-
-function App() {
-  return (
-    // <ApolloProvider client={client}>
-      // {/* Wrap page elements in Router component to keep track of location state */}
-      <Router>
-        <div>
-          <Header />
-          <div>
-
-            <Route exact path="/profile">
-              <Home />
-            </Route>
-
-            <Route exact path="/profile/profile">
-              <Profile />
-            </Route>
-
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    // </ApolloProvider>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
