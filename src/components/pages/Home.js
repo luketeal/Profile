bba81069378ba74b28758b7fe82ed3813f69063e
Luke Teal <l.teal21@gmail.com>
2021-08-04 16:02:33 -0400

    updating home content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import profileImg from '../assets/images/luketeal.jpg';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+    <main>
+        <article id="about" class="topmargin row-wrap">
+            
+            <div class="titlecard flex-column">
+                <h2>About me</h2>
+            </div>
+
+            <img 
+            src={profileImg}
+            alt="luke with a black jacket and a sky background"
+            class="mypicture"/>
+
+            <div class="aboutcontentwrap">
+                <p class="slogan">Build<span> {String.fromCharCode(183)} </span>Learn<span> {String.fromCharCode(183)} </span>Fun</p>
+                <p class="aboutcontent">A University of New Hampshire graduate in 2011, I
+                    have a passion for learning new things, having fun outdoors with my family,
+                    and traveling all around the world. The thing that has really gotten me going
+                    these days is coding and web development. I've been participating in a coding 
+                    bootcamp and I've enjoyed every minute of it.  Check out some of my work below.
+                    I'm always interested in meeting new folks and learning something new so reach
+                    out if you want! 
+                </p>
+            </div>
+        </article>
+    </main>
   );
 }
