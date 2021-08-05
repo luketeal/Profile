5b146325d6468aa3e74410079c56f82377808271
Luke Teal <l.teal21@gmail.com>
2021-08-05 11:41:04 -0400

    home defaults to about

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -8,10 +8,7 @@ function NavTabs({ currentPage, handlePageChange }) {
   return (
     <header className="header-wrap">
       <h1 className="name">
-        <a href="#home" onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+        <a href="#about" onClick={() => handlePageChange('About')}>        
             Luke Teal
         </a>
       </h1>
