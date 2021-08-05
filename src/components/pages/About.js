7d2a82185c219435ba223da890d05f4140babbde
Luke Teal <l.teal21@gmail.com>
2021-08-05 11:36:34 -0400

    add about content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import profileImg from '../assets/images/luketeal.jpg';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <main>
+        <article id="about" class="topmargin row-wrap">
+            
+            <div class="titlecard flex-column">
+                <h2>About me</h2>
+            </div>
+
+            <img 
+            src={profileImg}
+            alt="luke with a black jacket and a sky background"
+            class="mypicture"/>
+
+            <div class="aboutcontentwrap">
+                <p class="slogan">Build<span> {String.fromCharCode(183)} </span>Learn<span> {String.fromCharCode(183)} </span>Fun</p>
+                <p class="aboutcontent">A University of New Hampshire graduate in 2011, I
+                    have a passion for learning new things, having fun outdoors with my family,
+                    and traveling all around the world. The thing that has really gotten me going
+                    these days is coding and web development. I've been participating in a coding 
+                    bootcamp and I've enjoyed every minute of it.  Check out some of my work below.
+                    I'm always interested in meeting new folks and learning something new so reach
+                    out if you want! 
+                </p>
+            </div>
+        </article>
+    </main>
   );
 }
