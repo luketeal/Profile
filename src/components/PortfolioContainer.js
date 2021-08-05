af636d64c098a58933a38690a601ad329b0b9da9
Luke Teal <l.teal21@gmail.com>
2021-08-05 17:41:56 -0400

    create active class for navlinks

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,7 +6,7 @@ import Work from './pages/Work';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
